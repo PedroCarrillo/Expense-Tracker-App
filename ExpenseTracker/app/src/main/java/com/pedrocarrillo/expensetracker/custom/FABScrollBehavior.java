002c70c3eb7ca600cbff3bcab4ce5c2e96dd061b
Pedro Carrillo <pcarrillo@belatrixsf.com>
2015-10-06 09:10:18 -0500

    fix statistics scrolling behavior

--- ExpenseTracker/app/src/main/java/com/pedrocarrillo/expensetracker/custom/FABScrollBehavior.java
+++ ExpenseTracker/app/src/main/java/com/pedrocarrillo/expensetracker/custom/FABScrollBehavior.java
@@ -1,11 +1,17 @@
 package com.pedrocarrillo.expensetracker.custom;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.pedrocarrillo.expensetracker.ExpenseTrackerApp;
+import com.pedrocarrillo.expensetracker.R;
+import com.pedrocarrillo.expensetracker.ui.statistics.StatisticsFragment;
 
 /**
  * Created by Pedro on 9/19/2015.
@@ -19,6 +25,8 @@ public class FABScrollBehavior extends FloatingActionButton.Behavior {
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
             FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        String a = directTargetChild.getClass().getSimpleName();
+        if (target instanceof NestedScrollView && target.getTag() != null && target.getTag().toString().equalsIgnoreCase(ExpenseTrackerApp.getContext().getString(R.string.statistics))) return false;
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
             super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
     }
